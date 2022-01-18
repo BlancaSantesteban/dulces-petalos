6cfcdaf8f3747e00c9ede84db0f8a6efbeca01af
Blanca Santesteban <blanca.santesteban@biko2.com>
2022-01-18 09:50:42 +0100

    Pruebas inicio

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,39 @@
-import React from 'react';
+/* eslint-disable @typescript-eslint/no-redeclare */
+import { useEffect, useState } from 'react';
+import { Producto } from './Producto';
+import { Producto as ProductoComponent } from './components/Producto';
+import GlobalStyle from './styles/global';
 
 function App() {
-  return <>Hello, World!</>;
+  const [products, setProducts] = useState<Producto[]>([]);
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  const getProducts = async () => {
+    const response = await fetch(
+      'https://dulces-petalos.herokuapp.com/api/product',
+    );
+    const data = await response.json();
+    setProducts(data);
+  };
+
+  return (
+    <div className="App">
+      <GlobalStyle />
+      <ul>
+        {products.map((product: Producto) => (
+          <>
+            <ProductoComponent
+              key={product.id}
+              name={product.name}
+            ></ProductoComponent>
+          </>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
